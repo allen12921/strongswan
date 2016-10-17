47b85bc1c0cd0f9ce6194d07f61d8c70ae468542
mritd <mritd1234@gmail.com>
2016-10-17 18:10:44 +0800

    Update create_cert.sh

--- create_cert.sh
+++ create_cert.sh
@@ -12,21 +12,28 @@ ipsec pki --self --in ca.key.pem --dn "C=CN, O=StrongSwan, CN=StrongSwan CA" --c
 # create server certificate
 ipsec pki --gen --outform pem > server.key.pem
 ipsec pki --pub --in server.key.pem | ipsec pki --issue --cacert ca.cert.pem \
---cakey ca.key.pem --dn "C=CN, O=StrongSwan, CN=$vpn_host" \
---san="$vpn_host" --flag serverAuth --flag ikeIntermediate \
---outform pem > server.cert.pem
+  --cakey ca.key.pem --dn "C=CN, O=StrongSwan, CN=$vpn_host" \
+  --san="$vpn_host" --san="`ifconfig $1|sed -n 2p|awk  '{ print $2 }'|tr -d 'addr:'`" --flag serverAuth --flag ikeIntermediate \
+  --outform pem > server.cert.pem
 
 # create client certificate
 ipsec pki --gen --outform pem > client.key.pem
 ipsec pki --pub --in client.key.pem | ipsec pki --issue --cacert ca.cert.pem \
---cakey ca.key.pem --dn "C=CN, O=StrongSwan, CN=Client" \
---outform pem > client.cert.pem
+  --cakey ca.key.pem --dn "C=CN, O=StrongSwan, CN=Client" \
+  --outform pem > client.cert.pem
 
 # create pkcs12
 openssl pkcs12 -export -inkey client.key.pem -in client.cert.pem -name "Client" \
--certfile ca.cert.pem -caname "StrongSwan CA" -out client.cert.p12
+  -certfile ca.cert.pem -caname "StrongSwan CA" -out client.cert.p12
 
 # install certificate
+
+rm -f /etc/ipsec.d/cacerts/ca.cert.pem > /dev/null 2>&1
+rm -f /etc/ipsec.d/certs/server.cert.pem > /dev/null 2>&1
+rm -f /etc/ipsec.d/private/server.key.pem > /dev/null 2>&1
+rm -f /etc/ipsec.d/certs/client.cert.pem > /dev/null 2>&1
+rm -f /etc/ipsec.d/private/client.key.pem > /dev/null 2>&1
+
 cp -r ca.cert.pem /etc/ipsec.d/cacerts/
 cp -r server.cert.pem /etc/ipsec.d/certs/
 cp -r server.key.pem /etc/ipsec.d/private/
