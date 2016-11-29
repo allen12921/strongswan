9773bd2eddba0fa5a4e268b1c3f183b1cc59b0a5
mritd <mritd@mritd.me>
2016-11-29 11:44:25 +0800

    删除操作脚本

--- install.sh
+++ install.sh
@@ -37,18 +37,45 @@ wget https://download.strongswan.org/strongswan-$VERSION.tar.gz -O /tmp/strongsw
 && make -j \
 && make install)
 
-mkdir -p /etc/{strongswan,xl2tpd,ppp}
+# Delete tmp files
+rm -rf /tmp/strongswan* > /dev/null 2>&1
+
+# Kernel、Iptables setting
+sysctl -w net.ipv4.conf.all.rp_filter=2
+iptables --table nat --append POSTROUTING --jump MASQUERADE
+echo 1 > /proc/sys/net/ipv4/ip_forward
+for each in /proc/sys/net/ipv4/conf/*; do
+	echo 0 > $each/accept_redirects
+	echo 0 > $each/send_redirects
+done
 
 # Strongswan Configuration
+mkdir -p $CONFIGPATH
 cp ipsec.conf $CONFIGPATH/ipsec.conf
 cp strongswan.conf $CONFIGPATH/strongswan.conf
 
-# VPN Bin
-cp vpnctl /usr/local/bin/vpnctl
-cp vpn_adduser /usr/local/bin/vpn_adduser
-cp vpn_deluser /usr/local/bin/vpn_deluser
-cp vpn_setpsk /usr/local/bin/vpn_setpsk
-cp vpn_unsetpsk /usr/local/bin/vpn_unsetpsk
+# Generate a random password
+P1=`cat /dev/urandom | tr -cd abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789 | head -c 3`
+P2=`cat /dev/urandom | tr -cd abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789 | head -c 3`
+P3=`cat /dev/urandom | tr -cd abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789 | head -c 3`
+VPN_PASSWORD="$P1$P2$P3"
 
-# Delete tmp files
-rm -rf /tmp/strongswan* > /dev/null 2>&1
+# Generate a random password
+P1=`cat /dev/urandom | tr -cd abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789 | head -c 3`
+P2=`cat /dev/urandom | tr -cd abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789 | head -c 3`
+P3=`cat /dev/urandom | tr -cd abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789 | head -c 3`
+VPN_PSK="$P1$P2$P3"
+
+cat > $CONFIGPATH/ipsec.secrets <<EOF
+# This file holds shared secrets or RSA private keys for authentication.
+# RSA private key for this host, authenticating it to any other host
+# which knows the public part.  Suitable public keys, for ipsec.conf, DNS,
+# or configuration of other implementations, can be extracted conveniently
+# with "ipsec showhostkey".
+
+: RSA server.key.pem
+: PSK "$VPN_PSK"
+
+mritd : EAP "$VPN_PASSWORD"
+mritd : XAUTH "$VPN_PASSWORD"
+EOF
